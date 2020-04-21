2cf277a4d3e27c01b2156267222a6d25243c5c83
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2020-04-21 14:13:48 +0200

    Remove all files for the published build branch.

--- src/carrotsearch/ui/ButtonLink.js
+++
@@ -1,7 +0,0 @@
-import { Button } from "@blueprintjs/core";
-import React from 'react';
-
-export const ButtonLink = (props) => {
-  return <Button {...props} minimal={true} small={true}
-                 className={(props.className || "") + " link"} />;
-};
--- src/carrotsearch/ui/Optional.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Optional = (props) => {
-  return props.visible ? <>{props.content()}</> : null;
-};
